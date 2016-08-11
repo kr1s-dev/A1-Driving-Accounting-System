6f1c28f3d4028b08bce7d4ca4c2688f06debb444
Kristopher <kristopher.n.veraces@gmail.com>
2016-08-11 11:25:02 +0800

    To continue: invoice creation + journal entry

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -3,7 +3,7 @@
         <li class="user-details cyan darken-2">
         <div class="row">
           <div class="col col s4 m4 l4">
-            <img src="../images/avatar.jpg" alt="" class="circle responsive-img valign profile-image">
+            <img src=" {{ URL::asset('images/avatar.jpg')}}" alt="" class="circle responsive-img valign profile-image">
           </div>
           <div class="col col s8 m8 l8">
             <ul id="profile-dropdown" class="dropdown-content">
