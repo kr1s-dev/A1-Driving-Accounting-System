2b417e48f5564f7d74d0b827de517e0ea5529dcb
Kristopher Veraces <kristopher.n.veraces@gmail.com>
2016-08-23 00:16:57 +0800

    Update DatabaseSeeder.php
    Uncommented necessary value for database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,9 +10,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(UserTypeSeeder::class);
-        // $this->call(AccountGroupSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(UserTypeSeeder::class);
+        $this->call(AccountGroupSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
